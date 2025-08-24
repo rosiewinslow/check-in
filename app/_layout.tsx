59339a0b7b1f59ffa97f61e75759d5a39b124a5f
rosiewinslow <csylinda1301@gmail.com>
2025-08-24 18:51:50 +0900

    로그인, 계정 관리 로직 삭제(미사용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,5 @@
 // app/_layout.tsx
 import { Stack, useRouter, useSegments } from 'expo-router';
-import { useEffect } from 'react';
-import { ActivityIndicator, View } from 'react-native';
-import { AuthProvider, useAuth } from '../providers/AuthProvider';
 
 
 export default function RootLayout() {
@@ -11,32 +8,3 @@ export default function RootLayout() {
   );
 }
 
-
-/* 
-// 로그인 인증 분기 로직
-function ProtectedLayout() {
-  const { loading, userId } = useAuth();
-  const segments = useSegments();
-  const router = useRouter();
-  const inAuth = segments[0] === '(auth)';
-
-  useEffect(() => {
-    if (loading) return;
-    if (!userId && !inAuth) router.replace('/(auth)/sign-in');
-    if (userId && inAuth) router.replace('/(tabs)/dashboard');
-  }, [loading, userId, inAuth]);
-
-  if (loading) {
-    return <View style={{flex:1,justifyContent:'center',alignItems:'center'}}><ActivityIndicator/></View>;
-  }
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
-
-export default function RootLayout() {
-  return (
-    <AuthProvider>
-      <ProtectedLayout />
-    </AuthProvider>
-  );
-}
-*/
